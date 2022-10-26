882f499626dc7aa80b6eaf1fa91e6fbd85622ee3
denise.nanni <denise.nanni1@gmail.com>
2022-10-26 10:08:10 +0200

    62 benchmarks

--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,13 +1,19 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
  *
  */
 public final class UseListsAndMaps {
+    private static final int START = 1000;
+    private static final int END = 2000;
+    private static final int ELEMS = 100_000;
 
     private UseListsAndMaps() {
     }
@@ -21,19 +27,37 @@ public final class UseListsAndMaps {
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+
+        final List<Integer> arrayList = new ArrayList<>();
+
+        for (int i = START; i < END; i++) {
+            arrayList.add(i);
+        }
+
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+
+        final List<Integer> linkedList = new LinkedList<>(arrayList);
+        
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+
+        final int temp = arrayList.get(arrayList.size() - 1);
+        arrayList.set(arrayList.size() - 1, arrayList.get(0));
+        arrayList.set(0, temp);
+
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+        for (Integer val : arrayList) {
+            System.out.println(val);
+        }
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +65,27 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+        
+        System.out.println("ArrayList:");
+        insertionBenchmark(arrayList);
+
+        System.out.println("LinkedList:");
+        insertionBenchmark(linkedList);
+
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example PerfTest.java.
          */
+
+        System.out.println("ArrayList:");
+        readingBenchmark(arrayList);
+
+        System.out.println("LinkedList:");
+        readingBenchmark(linkedList); 
+        
+
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
@@ -66,5 +105,32 @@ public final class UseListsAndMaps {
         /*
          * 8) Compute the population of the world
          */
+    }
+
+    private static void insertionBenchmark(List<Integer> ls) {
+        long time = System.nanoTime();
+
+        for (int i = 1; i <= ELEMS; i++) {
+            ls.set(0, i);
+        }
+
+        time = System.nanoTime() - time;
+        var millis = TimeUnit.NANOSECONDS.toMillis(time);
+
+        System.out.println("Inserting " + ELEMS + " elements took " + time + "ns (" + millis + "ms)" );
+    }
+
+    private static void readingBenchmark(List<Integer> ls) {
+        long time = System.nanoTime();
+        int position = ls.size() / 2;
+
+        for (int i = 1; i <= 1000; i++) {
+            ls.get(position);
+        }
+        
+        time = System.nanoTime() - time;
+        long millis = TimeUnit.NANOSECONDS.toMillis(time);
+    
+        System.out.println("Reading 1000 elements took " + time + "ns (" + millis + "ms)" );
     }
 }
