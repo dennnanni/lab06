c0b5f507e8632453f1ff40e268090ec14eade850
denise.nanni <denise.nanni1@gmail.com>
2022-10-26 10:50:49 +0200

    63 check on fail probability added

--- 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
+++ 63-exceptions/src/main/java/it/unibo/exceptions/fakenetwork/impl/ServiceBehindUnstableNetwork.java
@@ -29,6 +29,10 @@ public final class ServiceBehindUnstableNetwork implements NetworkComponent {
         /*
          * The probability should be in [0, 1[!
          */
+        if (failProbability < 0 || failProbability > 1) {
+            throw new IllegalArgumentException("Field \"failProbability\" is out of range.");
+        }
+        
         this.failProbability = failProbability;
         randomGenerator = new Random(randomSeed);
     }
